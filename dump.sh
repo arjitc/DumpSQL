78571bb5eff598cd46e1ea3a529adc9a70e33a41
Arjit Chaudhary <arjit.c@gmail.com>
2014-05-22 21:24:09 +0530

    Added SCP function to remotely backup the DB.

--- dump.sh
+++ dump.sh
@@ -1,7 +1,14 @@
 #!/bin/bash   
+
+#dont forget to set these below or else this script will not work
 database_name=""
 database_user=""
 database_password=""
+
+#details for SCP
+remote_host=""
+remote_host_username=""
+remote_host_password=""
 
 # Define a timestamp function
 timestamp() {
@@ -9,6 +16,10 @@ timestamp() {
 }
 
 # lets dump the DB now
-mysqldump -u $database_user -p$database_password $database_name > $(timestamp).sql
+filename=$(timestamp)
+remote_filename=$filename.sql
+mysqldump -u $database_user -p$database_password $database_name > $filename.sql
 
+# lets scp this!
+scp $remote_filename $remote_host_username@$remote_host:/root
 echo "Done!"
